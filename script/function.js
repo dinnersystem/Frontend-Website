80914bb44f90e24dd4b7b658ccca1d9860e8597f
lawrence910426 <lawrence910426@gmail.com>
2020-03-12 15:17:21 +0800

    allow multiple organization

--- script/function.js
+++ script/function.js
@@ -5,26 +5,8 @@ var url = "/dinnersys_beta/backend/backend.php?";
 
 function login(uid, pswd, org_id, callback, done) {
     var json;
-<<<<<<< Updated upstream
-
-    $.get(url + "cmd=login&id=" + uid + "&password=" + pswd + "&device_id=website", function (data) {
-        json = data;
-    }).done(function () {
-        callback(json);
-        done();
-    });
-}
-
-function logout(done) {
-    $.get(url + "cmd=logout", function (data) {
-
-    }).done(function () {
-        done();
-    });
-=======
     $.post(url, { cmd: "login", "org_id": org_id, "id": uid, "password": pswd, "device_id": "website" }, (data) => { json = data; })
     .done(() => { callback(json); done(); });
->>>>>>> Stashed changes
 }
 
 
@@ -108,10 +90,7 @@ function get_money(callback) {
         callback(result);
     });
 }
-<<<<<<< Updated upstream
-=======
 
 function change_password(old_pass, new_pass, callback) { $.post(url, { "cmd": "change_password", "old_pswd": old_pass, "new_pswd": new_pass }, callback); }
 function select_order(start, end, callback) { $.post(url, { "cmd": "select_self", "esti_start": start, "esti_end": end, "history": "true" }, callback); }
 function show_org(callback) { $.post(url, { "cmd": "show_organization" }, callback); }
->>>>>>> Stashed changes
